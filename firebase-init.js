9bbf414dc253b6602e08db35b808b954af63cea5
Kemily-Ianz <kemily.ianz642@gmail.com>
2025-11-07 17:14:22 +0000

    a

---
+++ firebase-init.js
@@ -0,0 +1,9 @@
+ const firebaseConfig = {
+    apiKey: "AIzaSyCd1Oh_mRyQlLY-CTioB1n33tRwlnA9kLU",
+    authDomain: "projeto-login-8cc14.firebaseapp.com",
+    projectId: "projeto-login-8cc14",
+    storageBucket: "projeto-login-8cc14.firebasestorage.app",
+    messagingSenderId: "755320659151",
+    appId: "1:755320659151:web:7016dc7feaf2da5f8cebba",
+    measurementId: "G-F2B1413THP"
+  };
